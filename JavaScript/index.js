5de5ac798d3893c67e11a23267365807f4929ee5
arta kerqeli <dardanxhymshiti@dardans-MacBook-Pro.local>
2023-09-09 14:48:56 +0200

    Add javascrip and typescript solution

---
+++ JavaScript/index.js
@@ -0,0 +1,2 @@
+const name= "arta";
+console.log(name);
--- TypeScript/types.d.ts
+++ TypeScript/types.d.ts
@@ -1,1 +1,28 @@
+enum Domain {
+    WEB="Web",
+    Connect="Connect",
+    Backend="Backend"
+}
 
+interface Interns {
+    name: string,
+    age: number,
+    skills: string[];
+}
+
+interface Juniors{
+    name: string, 
+    age: number, 
+    skills: string[],
+    date_of_promotion: Date,
+    domain: Domain,
+}
+
+interface Company {
+    Interns: Interns[];
+    Juniors: Juniors[];
+    Country: string;
+}
+  
+  // Export the Company interface
+  export { Company }
